15f783eb52d01ab41ca144beaed1f2bd7138b87f
如彼 <hua.qiuh@alibaba-inc.com>
2015-02-10 13:52:01 +0800

    improve command path resolver, hiding unneccesory error messages

--- lib/lfd/env.rb
+++ lib/lfd/env.rb
@@ -9,84 +9,86 @@ module LFD
       check_flash_player
     end
 
-    def check_flex_sdk
-      check_cmd( "flex sdk - mxmlc", mxmlc_path )
-      check_cmd( "flex sdk - compc", compc_path )
-    end
+    protected
 
-    def check_cmd title, cmd_path
-      print "#{title}: ".capitalize.rjust(20)
-      if cmd_path
-        puts cmd_path.chomp.green
-      else
-        puts "✗".red
+      def check_flex_sdk
+        check_cmd( "flex sdk - mxmlc", mxmlc_path )
+        check_cmd( "flex sdk - compc", compc_path )
       end
-    end
 
-    def check_flash_player
-      check_cmd 'Flash Player', fp_path
-    end
+      def check_cmd title, cmd_path
+        print "#{title}: ".capitalize.rjust(20)
+        if cmd_path
+          puts cmd_path.chomp.green
+        else
+          puts "✗".red
+        end
+      end
 
-    def flex_sdk_ready?
-      mxmlc_ready? && compc_ready?
-    end
+      def check_flash_player
+        check_cmd 'Flash Player', fp_path
+      end
 
-    def mxmlc_ready?
-      executable? mxmlc
-    end
+      def flex_sdk_ready?
+        mxmlc_ready? && compc_ready?
+      end
 
-    def executable? cmd
-      !!executable_path( cmd )
-    end
+      def mxmlc_ready?
+        executable? mxmlc
+      end
 
-    def executable_path cmd
-      path = `which #{Shellwords.escape(cmd)}`
-      path.empty? ? nil : path.chomp
-    end
+      def executable? cmd
+        !!executable_path( cmd )
+      end
 
-    def compc_ready?
-      executable? compc
-    end
+      def executable_path cmd
+        path = IO.popen(['which', Shellwords.escape(cmd)], err: '/dev/null').read.chomp!
+        path.empty? ? nil : path
+      end
 
-    def mxmlc
-      ENV['MXMLC'] || 'mxmlc'
-    end
+      def compc_ready?
+        executable? compc
+      end
 
-    def mxmlc_path
-      @mxmlc_path ||= executable_path( mxmlc )
-    end
+      def mxmlc
+        ENV['MXMLC'] || 'mxmlc'
+      end
 
-    def compc
-      ENV['COMPC'] || 'compc'
-    end
+      def mxmlc_path
+        @mxmlc_path ||= executable_path( mxmlc )
+      end
 
-    def compc_path
-      @compc_path ||= executable_path( compc )
-    end
+      def compc
+        ENV['COMPC'] || 'compc'
+      end
 
-    def flash_player_ready?
-      executable? flash_player
-    end
+      def compc_path
+        @compc_path ||= executable_path( compc )
+      end
 
-    alias_method :fp_ready?, :flash_player_ready?
+      def flash_player_ready?
+        executable? flash_player
+      end
 
-    def flash_player
-      ENV['FLASH_PLAYER'] || 'flashplayer'
-    end
+      alias_method :fp_ready?, :flash_player_ready?
 
-    def flash_player_path
-      @fp_path ||= executable_path( flash_player )
-    end
+      def flash_player
+        ENV['FLASH_PLAYER'] || 'flashplayer'
+      end
 
-    alias_method :fp_path, :flash_player_path
+      def flash_player_path
+        @fp_path ||= executable_path( flash_player )
+      end
 
-    def trace_log_file
-      File.join ENV['HOME'], '.macromedia/Flash_Player/Logs/flashlog.txt'
-    end
+      alias_method :fp_path, :flash_player_path
 
-    def mm_cfg
-      File.join ENV['HOME'], 'mm.cfg'
-    end
+      def trace_log_file
+        File.join ENV['HOME'], '.macromedia/Flash_Player/Logs/flashlog.txt'
+      end
+
+      def mm_cfg
+        File.join ENV['HOME'], 'mm.cfg'
+      end
 
   end
 
